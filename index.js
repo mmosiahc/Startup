7c1bb63179c29a9c35db5e4a77377b204bd5bf25
Michael Crow <mmosiahc@gmail.com>
2023-12-21 22:56:28 -0600

    Adding express endpoints

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 const express = require('express');
 const app = express();
 
-// The service port. In production the frontend code is statically hosted by the service on the same port.
+// The service port. In production the front-end code is statically hosted by the service on the same port.
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
 // JSON body parsing using built-in middleware
 app.use(express.json());
 
-// Serve up the frontend static content hosting
+// Serve up the front-end static content hosting
 app.use(express.static('public'));
 
 // Router for service endpoints
-const apiRouter = express.Router();
+var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
-// GetScores
-apiRouter.get('/scores', (_req, res) => {
-  res.send(scores);
+// Get your messages
+apiRouter.get('/messages', (_req, res) => {
+  res.send(messages);
 });
 
-// SubmitScore
-apiRouter.post('/score', (req, res) => {
-  scores = updateScores(req.body, scores);
-  res.send(scores);
+// Submit a lovebox message
+apiRouter.post('/messages', (req, res) => {
+  messages = updateMessages(req.body, messages);
+  res.send(messages);
 });
 
 // Return the application's default page if the path is unknown
@@ -33,3 +33,27 @@ app.use((_req, res) => {
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+// updateMessages considers a new score for inclusion in the high scores.
+// The high scores are saved in memory and disappear whenever the service is restarted.
+let messages = [{ date: "January", from: "michael", message :"I have 4 kids already", time: "08:00am"}];
+function updateMessages(newMessage, messages) {
+  let found = false;
+  for (const [i, prevMessage] of messages.entries()) {
+    if (newMessage.message > prevMessage.message) {
+      messages.splice(i, 0, newMessage);
+      found = true;
+      break;
+    }
+  }
+
+  if (!found) {
+    messages.push(newMessage);
+  }
+
+  if (messages.length > 10) {
+    messages.length = 10;
+  }
+
+  return messages;
+}
